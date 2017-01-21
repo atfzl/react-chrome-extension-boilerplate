166ff68fb6a5b644f8485263a5632127a070a24d
Atif Afzal <atif.afzal@paytm.com>
2017-01-21 22:24:44 +0530

    Adding styled-components and flow-typed

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,12 +1,17 @@
 // @flow
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color: red;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <StyledDiv>
         Hello
-      </div>
+      </StyledDiv>
     );
   }
 }
