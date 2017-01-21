ac5f6c54db68e9734f51bd32014e6798dd999f1f
Atif Afzal <atif.afzal@paytm.com>
2017-01-21 21:26:37 +0530

    Adding flow and tcomb

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,11 @@
+// @flow
 import React, { Component } from 'react';
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+add(1, 'a');
 
 export default class App extends Component {
   render() {
